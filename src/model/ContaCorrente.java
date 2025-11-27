60c18cf996ab34be84373da149a608ab98bf9e94
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-11-27 08:58:49 -0300

    Tratando exceção do limite de operação no depósito

--- src/model/ContaCorrente.java
+++ src/model/ContaCorrente.java
@@ -1,4 +1,49 @@
 package model;
 
+import exceptions.LimiteExcedidoException;
+
 public class ContaCorrente {
+    private int numero;
+    private String titular;
+    private float saldo;
+
+    public ContaCorrente(int numero, String titular, float saldo) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean depositar(float valor) throws LimiteExcedidoException{
+        if(valor > 10000){
+            throw new LimiteExcedidoException(
+                    "Limite de transação excedido");
+        }
+        saldo += valor;
+        return true;
+    }
+
 }
