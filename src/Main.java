11693a944ad5760eb2628a86015bc5e23cb055de
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-11-27 07:48:09 -0300

    Exemplo de pilha de execução

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        metodo();
+    }
+
+    private static void metodo() {
+        novoMetodo();
+    }
+
+    private static void novoMetodo() {
+        int array[] = null;
+        System.out.println(array.length);
+    }
+
+}
